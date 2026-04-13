ff5133015db09d3ed5b941dbbda148b7c27f7a25
javiera69 <javiera.betanzos@murciaeduca.es>
2026-04-13 14:40:44 +0200

    Change base path from '/App-de-escaneo/' to '/App-Scan/'

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: '/App-de-escaneo/',
+    base: '/App-Scan/',
     server: {
       port: 3000,
       host: '0.0.0.0',
